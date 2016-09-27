8f6b3639ffd4f8aa5ce8847ae450984d31f5ba47
MendelMonteiro <mendel.monteiro@gmail.com>
2016-09-27 23:17:26 +0200

    Add a test client to be able to see how the web api will be used

---
+++ BookARoom.TestClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookARoom.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:60416/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            GetValue(client).Wait();
+
+            Console.ReadKey();
+        }
+
+        private static async Task GetValue(HttpClient client)
+        {
+            try
+            {
+                // TODO: call real api when it exists
+                var response = await client.GetAsync("api/values/2");
+                if (response.IsSuccessStatusCode)
+                {
+                    var stuff = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(stuff);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
